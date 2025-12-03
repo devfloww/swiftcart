235af4aa4a6d7798047ec980dd37bc37aba5405f
Sadiq Hayatuddeen <svdiqhayatuddeen@gmail.com>
2025-12-03 17:27:27 +0100

    Added users router

--- core/main.py
+++ core/main.py
@@ -4,9 +4,8 @@ from sqlalchemy.ext.asyncio import AsyncSession
 # from sqlalchemy import text # for debug
 
 from core.db.database import get_db, engine
-from core.db.schemas import UserCreate, UserRead
 from core.db import models
-from core.utils.security import hash_password
+from core.routes.users import router as users_router
 
 # Fastapi lifespan
 @asynccontextmanager
@@ -28,40 +27,16 @@ app = FastAPI(
     docs_url="/api/docs/",
 )
 
+# Routers
+app.include_router(users_router)
 
-@app.get("/api")
+# Root Endpoint
+@app.get("/")
 async def root():
     return {"message": "Swiftcart core API is running."}
-
 
 ### THESE ARE FOR QUICK TESTS
 @app.get("/check_db")
 async def check_db(db: AsyncSession = Depends(get_db)):
     result = await db.execute(text("SELECT 1"))
     return {"db_connection": result.scalar()}
-
-@app.post("/api/users/create", response_model=UserRead, status_code=status.HTTP_201_CREATED)
-async def create_user(user_in: UserCreate, db: AsyncSession = Depends(get_db)):
-    existing_user = await db.execute(
-        models.select(models.User).where(models.User.email == user_in.email.lower())
-    )
-    if existing_user.scalar() is not None:
-        raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST,
-            detail="Email already registered."
-        )
-    password_hash = hash_password(user_in.password)
-    new_user = models.User(
-        first_name=user_in.first_name.strip().title(),
-        last_name=user_in.last_name.strip().title(),
-        email=user_in.email.lower(),
-        password_hash=password_hash,
-        role=user_in.role
-    )
-    
-    # DB trns
-    db.add(new_user)
-    await db.commit()
-    await db.refresh(new_user)
-    
-    return new_user
---
+++ core/routes/users.py
@@ -0,0 +1,46 @@
+from fastapi import APIRouter, Depends, HTTPException, status
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from core.db.database import get_db, engine
+from core.db.schemas import UserCreate, UserRead
+from core.db import models
+from core.utils.security import hash_password
+
+# Users Router
+router = APIRouter(
+    prefix="/api/users",
+    tags=["users"],
+    responses={
+        404: {"description": "Not found"},
+    }
+)
+
+@router.post("/create", response_model=UserRead, status_code=status.HTTP_201_CREATED)
+async def create_user(user_in: UserCreate, db: AsyncSession = Depends(get_db)):
+    existing_user = await db.execute(
+        models.select(models.User).where(models.User.email == user_in.email.lower())
+    )
+    if existing_user.scalar() is not None:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Email already registered."
+        )
+    password_hash = hash_password(user_in.password)
+    new_user = models.User(
+        first_name=user_in.first_name.strip().title(),
+        last_name=user_in.last_name.strip().title(),
+        email=user_in.email.lower(),
+        password_hash=password_hash,
+        role=user_in.role
+    )
+    
+    # DB trns
+    db.add(new_user)
+    await db.commit()
+    await db.refresh(new_user)
+    
+    return new_user
+
+@router.get("/me", response_model=UserRead)
+async def read_users_me():
+    return {"message": "This endpoint will return the current authenticated user's details."}

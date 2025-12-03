071473049ff3a9b4cfe09f2c345bd7290042376e
Sadiq Hayatuddeen <svdiqhayatuddeen@gmail.com>
2025-12-03 15:39:25 +0100

    Added User, Vendor models

---
+++ core/db/models.py
@@ -0,0 +1,64 @@
+from email.policy import default
+from sqlalchemy import (
+    Column,
+    ForeignKey
+    String,
+    Integer,
+    Numeric,
+    DateTime,
+    Boolean,
+    Enum as SQLEnum,
+    func
+)
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
+from datetime import datetime, timezone
+import enum
+from decimal import Decimal
+
+class Base(DeclarativeBase):
+    pass
+
+class UserRole(enum.StrEnum):
+    CUSTOMER = "customer"
+    VENDOR = "vendor"
+    ADMIN = "admin"
+    
+class User(Base):
+    __tablename__ = "users"
+    
+    id: Mapped[int] = mapped_column(primary_key=True, index=True)
+    first_name: Mapped[str] = mapped_column(String(50), nullable=False)
+    last_name: Mapped[str] = mapped_column(String(50), nullable=False)
+    email: Mapped[str] = mapped_column(String(100), unique=True, nullable=False)
+    password_hash: Mapped[str] = mapped_column(String(255), nullable=False)
+    avatar_url: Mapped[str] = mapped_column(String(500), nullable=True)
+    is_verified: Mapped[bool] = mapped_column(Boolean, default=False)
+    created_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), server_default=func.now())
+    updated_at: Mapped[datetime]= mapped_column(DateTime(timezone=True), server_default=func.now(), onupdate=func.now())
+    role: Mapped[UserRole] = mapped_column(SQLEnum(UserRole), default=UserRole.CUSTOMER)
+    
+    # relationships
+    vendor: Mapped["Vendor"] = relationship("Vendor", back_populates="user", uselist=False)
+    
+class Vendor(Base):
+    __tablename__ = "vendors"
+    
+    id: Mapped[int] = mapped_column(primary_key=True, index=True)
+    user_id: Mapped[int] = mapped_column(Integer,ForeignKey("users.id", ondelete="CASCADE"), nullable=False, index=True, unique=True)
+    store_name: Mapped[str] = mapped_column(String(100), nullable=False)
+    store_slug: Mapped[str] = mapped_column(String(100), unique=True, nullable=False)
+    address: Mapped[str] = mapped_column(String(255), nullable=False)
+    contact_number: Mapped[str] = mapped_column(String(20), nullable=False)
+    created_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), server_default=func.now())
+    updated_at: Mapped[datetime]= mapped_column(DateTime(timezone=True), server_default=func.now(), onupdate=func.now())
+    description: Mapped[str] = mapped_column(String(1000), nullable=True)
+    logo_url: Mapped[str] = mapped_column(String(500), nullable=True)
+    banner_url: Mapped[str] = mapped_column(String(500), nullable=True)
+    is_approved: Mapped[bool] = mapped_column(Boolean, default=False, nullable=True)
+    approved_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), server_default=func.now())
+    balance: Mapped[Decimal] = mapped_column(
+        Numeric(12, 2), default=Decimal("0.00"), server_default="0.00", nullable=False
+    )
+    
+    # relationships
+    user: Mapped["Ueser"] = relationship("User", back_populates="vendor", uselist=False)
